30471fad730e4966d8836a39df5250be3de50e19
zatlawy@wincs.cs.bgu.ac.il <zatlawy@post.bgu.ac.il>
2020-01-15 13:22:05 +0200

    Eli - small fixes

--- spl-net/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
+++ spl-net/src/main/java/bgu/spl/net/impl/stomp/StompServer.java
@@ -2,17 +2,37 @@ package bgu.spl.net.impl.stomp;
 
 import bgu.spl.net.api.MessageEncoderDecoderImpl;
 import bgu.spl.net.api.StompMessagingProtocolImpl;
+import bgu.spl.net.impl.rci.ObjectEncoderDecoder;
+import bgu.spl.net.impl.rci.RemoteCommandInvocationProtocol;
 import bgu.spl.net.srv.Server;
 
 public class StompServer {
 
     public static void main(String[] args) {
+
+        short port = (short)Integer.parseInt(args[0]);
+        if(args[1].equals("tpc"))
+            runThreadPerClientServer(port);
+        else
+            runReactorServer(port);
+    }
+
+    private static void runThreadPerClientServer(short port){
         Server.threadPerClient(
-                2302, //port
+                port, //port
+                StompMessagingProtocolImpl::new, //protocol factory
+                MessageEncoderDecoderImpl::new //message encoder decoder factory
+        ).serve();
+    }
+    private static void runReactorServer(short port){
+        Server.reactor(
+                Runtime.getRuntime().availableProcessors(),
+                port, //port
                 StompMessagingProtocolImpl::new, //protocol factory
                 MessageEncoderDecoderImpl::new //message encoder decoder factory
         ).serve();
 
     }
+
 }
 
